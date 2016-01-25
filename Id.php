846d86dfbc09ec6c21593935c8b99f3215a75ccf
karel <osorioramirez@gmail.com>
2016-01-25 16:31:01 +0000

    added entity & string value obkect

---
+++ Id.php
@@ -0,0 +1,15 @@
+<?php
+
+/**
+ * This file is part of the cubiche/cubiche project.
+ */
+namespace Cubiche\Domain\Core;
+
+/**
+ * Abstract Id Class.
+ *
+ * @author Karel Osorio Ramírez <osorioramirez@gmail.com>
+ */
+abstract class Id extends NativeValueObject implements IdInterface
+{
+}
